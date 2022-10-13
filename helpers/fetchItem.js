002a48329d1d384538ac1fa89ed248eba7d4743c
Bernardo Martorano <bernardormartorano@gmail.com>
2022-10-13 11:33:56 -0300

    Requisito 03 concluído

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,14 @@
-const fetchItem = () => {
-  // seu código aqui só pro commit. 
+const fetchItem = async (itemFound) => {
+  if (itemFound === undefined) {
+    throw new Error('You must provide an url');
+  }
+  try {
+    const request = await fetch(`https://api.mercadolibre.com/items/${itemFound}`);
+    const result = await request.json();
+    return result;
+  } catch (error) {
+    // captura o erro
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,22 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se a função é de fato uma função.', () => {
+    expect(typeof fetchItem).toBe('function');
+  })
+  it('Com o argumento a função fetchItem chama a fetch.', async () => {
+    await fetchItem('MLB1615760527');
+    expect(global.fetch).toHaveBeenCalled();
+  })
+  it('Com o argumento, a função fetchItem faz com que a fetch utiliza o endpoinst correto', async () => {
+    await fetchItem('MLB1615760527');
+    const correctUrl = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(global.fetch).toHaveBeenCalledWith(correctUrl);
+  })
+  it('Com o argumento, fetchProducts retorna o objeto correto.', async () => {
+    await expect(fetchItem('MLB1615760527')).resolves.toEqual(item);
+  })
+  it('Sem parâmetros, a função deve lançar um erro.', async () => {
+    await expect(fetchItem()).rejects.toThrow('You must provide an url');
+  })
 });
