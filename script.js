621fc9c21384098e065e6e634b9512006e15c870
Bernardo Martorano <bernardormartorano@gmail.com>
2022-10-18 11:45:03 -0300

    Requisito 10 concluído e lint corrigido

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const createProductItemElement = (object) => {
  * @param {Element} product - Elemento do produto.
  * @returns {string} ID do produto.
  */
-const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
+// const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
 
 /**
  * Função responsável por criar e retornar um item do carrinho.
